b6eb34c0fe6e199a66c98dbd9462a156bd0d662b
Carl Hofmeister <hofmeister.carl@gmail.com>
2019-12-19 01:01:57 -0600

    Change name of dotfile manager, module list format, location of data files

--- modconfig.sh
+++
@@ -1,8 +0,0 @@
-enabled_modules=(
-    "git@github.com:ottopasuuna/dot-vim.git"
-    "git@github.com:ottopasuuna/dot-zsh.git"
-    "git@github.com:ottopasuuna/dot-misc.git"
-    "git@github.com:ottopasuuna/dot-xorg.git"
-    "git@github.com:ottopasuuna/dot-awesome.git"
-    "git@github.com:ottopasuuna/dot-systemd.git"
-)
